0f4a9c4b0c12e1bbc52f846073d37cb65757908e
JaelAguilar <cjaelaguilar@gmail.com>
2022-12-05 15:14:28 -0600

    Added solution 2

--- Day 1/solution.py
+++ Day 1/solution.py
@@ -10,3 +10,18 @@ def solution1():
                     maxCalories=currentCalories
                 currentCalories=0        
     print(maxCalories)
+
+def solution2():
+    maxCalories=[]
+    currentCalories=0
+    for line in inputText:
+        try:
+            currentCalories+=int(line)
+        except:
+            if line=="\n":
+                maxCalories.append(currentCalories)
+                currentCalories=0  
+    maxCalories.sort(reverse=True)     
+    print(maxCalories)
+    print(sum(maxCalories[0:3]))
+
