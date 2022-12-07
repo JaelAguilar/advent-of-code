c137be60c1c2f7712fb8b1e7b375d2f909ff08e0
JaelAguilar <cjaelaguilar@gmail.com>
2022-12-07 10:22:51 -0600

    Day 7 Solution 1

--- Day 7/solution1.py
+++ Day 7/solution1.py
@@ -1,21 +1,25 @@
-from anytree import Node,RenderTree,find_by_attr,PreOrderIter
+from anytree import Node,RenderTree,findall_by_attr,PreOrderIter
 import re
 inputText=open("Day 7//input.txt","r")
 root=Node('/')
+parent=root
 
 for line in inputText:
     line=line.replace('\n','')
     # Command
-    if "$ ls" in line:continue
-    if "cd" in line:
-        parent=line.replace('$ cd ','')
+    if ("$ ls" in line) or ("$ cd /" in line):continue
+    elif "$ cd .." in line:
+        parent=parent.parent
+        
+    elif "$ cd" in line:
+        parent=findall_by_attr(parent,line.replace('$ cd ',''),maxlevel=2)[-1]
     # Output
-    elif "dir" in line:
+    elif "dir " in line:
         folder=line.replace('dir ','')
-        Node(folder,parent=find_by_attr(root,parent))
+        Node(folder,parent=parent)
     else:
         file=re.findall('\d+',line)[0]
-        Node(file,parent=find_by_attr(root,parent))
+        Node(file,parent=parent)
         
 for pre, _, node in RenderTree(root):
         print("%s%s" % (pre, node.name))
