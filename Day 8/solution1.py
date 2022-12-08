e59052935819f117d425172b4c464208c34088df
JaelAguilar <cjaelaguilar@gmail.com>
2022-12-08 16:05:57 -0600

    Deleted unecessary code

--- Day 8/solution1.py
+++ Day 8/solution1.py
@@ -3,16 +3,13 @@ inputText=open("Day 8//input.txt","r")
 count=0
 grid=np.array([])
 
-#Creating array
 arrayLength=0
 for line in inputText:
     arrayLength=len(line)-1
     for j in range(len(line)-1):
         grid=np.append(grid,int(line[j]))
 grid=grid.reshape(arrayLength,arrayLength)
-print(grid.shape)
 
-# Analyze trees
 for row in range(arrayLength):
     for column in range(arrayLength):
         if row==0 or row==arrayLength-1 or column==0 or column==arrayLength-1:
