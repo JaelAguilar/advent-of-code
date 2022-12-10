25706b9336732758ec5e5c468e346395eaf7fc54
JaelAguilar <71282327+JaelAguilar@users.noreply.github.com>
2022-12-10 14:33:45 -0600

    Functioned more or less, the first and last pixels are buggy

--- Day 10/solution2.py
+++ Day 10/solution2.py
@@ -1,6 +1,5 @@
 inputText=open("Day 10//input.txt","r").read().split('\n')
 
-#lines=[x for x in open("Day 9//input.txt").read().split('\n')]
 cycles=0
 X=1
 importantCycles=[40,80,120,160,200,240]
@@ -17,22 +16,12 @@ def moveSignal(line):
         return move
     
 def drawSignal(sprite,X,CRT):
-    for i in range(len(importantCycles)):
-        if sprite[1]<importantCycles[i]:
-            verticalIndex=i
-    if sprite[1] in passedCheck:
-        return CRT
-    
-    print("v:",verticalIndex)
-    print('max:',max(0,sprite[1]//40))
-    CRT[max(0,sprite[1]//40)].append('#' if X in [i%40 for i in sprite] else '.')
-    passedCheck.append(sprite[0])
+    CRT[sprite[1]//40].append('#' if X in [i%40 for i in sprite] else '.')
     return CRT
     
 
 for line in inputText:
     sprite=[cycles+i for i in [-1,0,1]]
-    print(sprite)
     if line=="noop":
         #Start cycle noop
         CRT=drawSignal(sprite,X,CRT)
@@ -40,17 +29,15 @@ for line in inputText:
         
         #End cycle
     else:
-        print("Cycle in:",cycles,"X:",X)
         # Start first cycle addx
-        CRT=drawSignal(sprite,X,CRT)
+        CRT=drawSignal(sprite,X,CRT)                                 
         cycles+=1
         # End cycle
         #Start second cycle addx
+        sprite=[cycles+i for i in [-1,0,1]]
         CRT=drawSignal(sprite,X,CRT)
         cycles+=1
         #End cycle
         X+=moveSignal(line)
-
-    print("Cycle out:",cycles,"X:",X)
     
 print('\n'.join([''.join(i) for i in CRT]))
